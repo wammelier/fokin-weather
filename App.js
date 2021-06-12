7562c11f34ea4180903eb1dcabf5ddd8b773a079
이승환 <royhan4417@gmail.com>
2021-06-12 15:04:50 +0900

    expo 에서 지원하는 location 사용해봄

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Loading from "./Loading"
+import {Alert} from "react-native";
+import Loading from "./Loading";
+import * as Location from 'expo-location';
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  getLocation = async() => {
+    try {
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location)
+    }catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+  componentDidMount() {
+      this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
+
 }
