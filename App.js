22fa84e20ac625254baef86df9f3951c8dfd8ae5
이승환 <royhan4417@gmail.com>
2021-06-12 10:38:58 +0900

    reactNative styleSheet 적용방법 배우기

--- App.js
+++ App.js
@@ -1,27 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}></View>
-      <View style={styles.blueView}></View>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  yellowView: {
-    flex: 20,
-    backgroundColor: "yellow"
-  },
-  blueView: {
-    flex: 30,
-    backgroundColor: "blue"
-  }
-});
